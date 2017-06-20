c0a04f60ea135ef97541beef0d25834778bfdb57
adaGenius <451163293@qq.com>
2017-06-20 21:55:10 +0800

    是不是六

--- testCommand/testCommand/ViewController.m
+++ testCommand/testCommand/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = [UIColor orangeColor];
+    self.view.backgroundColor = [UIColor orangeColor];//666
 }
 
 
